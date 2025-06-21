7dacb50f4c51f6dea9139f7e3e1dead5801382dd
metissskz <132703265+metissskz@users.noreply.github.com>
2025-06-21 19:00:30 +0500

    Create states.py

---
+++ states.py
@@ -0,0 +1,7 @@
+from aiogram.fsm.state import StatesGroup, State
+
+class FenceCalc(StatesGroup):
+    choosing_type = State()
+    entering_length = State()
+    entering_gates = State()
+    done = State()
