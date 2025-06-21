a00072cf879dd78467e8940e48732d528595dfc8
metissskz <132703265+metissskz@users.noreply.github.com>
2025-06-21 18:06:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from aiogram import Bot, Dispatcher, types, executor
 import logging
 import os
+from dotenv import load_dotenv
 
-API_TOKEN = os.getenv("BOT_TOKEN") or "PASTE_YOUR_TOKEN_HERE"
+load_dotenv()
+
+API_TOKEN = os.getenv("BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
@@ -10,8 +13,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
-    await message.answer("Добро пожаловать в ZABOROFF калькулятор!
-Введите длину забора в метрах:")
+    await message.answer("Добро пожаловать в ZABOROFF калькулятор!\nВведите длину забора в метрах:")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
