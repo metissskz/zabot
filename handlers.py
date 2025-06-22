7d8e3ae617ee7390b39184be7186d34db9933bb9
metissskz <132703265+metissskz@users.noreply.github.com>
2025-06-22 15:15:55 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -25,7 +25,7 @@ main_menu = ReplyKeyboardMarkup(
 async def cmd_start(message: types.Message, state: FSMContext):
     await state.clear()
     await message.answer(
-        "Привет! Я бот ZaborOFF — помогу рассчитать забор и сформировать КП.
+        "Привет! Я бот ZaborOFF — помогу рассчитать забор и сформировать КП."
 Выберите действие:",
         reply_markup=main_menu
     )
