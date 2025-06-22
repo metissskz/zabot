42af5941c9cfa6ad13ffb9bf8204c0d2c8386fdc
metissskz <132703265+metissskz@users.noreply.github.com>
2025-06-22 14:57:16 +0500

    Update states.py

--- states.py
+++ states.py
@@ -1,11 +1,7 @@
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.state import StatesGroup, State
 
 class FenceCalc(StatesGroup):
     choosing_type = State()
-    choosing_post_type = State()
-    asking_length = State()
-    asking_gate = State()
-    asking_foundation = State()
-    asking_found_length = State()
-    asking_found_width = State()
-    asking_found_height = State()
+    entering_length = State()
+    foundation = State()
+    slope = State()
